27d5bb30db3734871e33931e19c8a4902c7948a7
zondezatera <zatera1920@gmail.com>
2016-05-10 15:38:32 +0700

    Update function about get data by sub url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function() {
-		jQuery('#export_csv').hide();
+		jQuery('#export_csv,.sk-fading-circle').hide();
 		jQuery('#generate_csv').click(function (e) {
 			var feed_url  = jQuery("#rss_url").val();
 			if (feed_url.length == 0) {
@@ -28,22 +28,52 @@ jQuery(document).ready(function() {
 									}
 									array_content.push(array_data);
 								});
-								console.log(array_content);
-								// Pattern Data
-								var csvContent = "data:text/csv;charset=utf-8,";
+								// console.log(array_content);
+								// Pattern Full Data 
+								var full_data_csv = "data:text/csv;charset=utf-8,";
 								array_content.forEach(function(infoArray, index){
 										 dataString = infoArray.join(",");
 										 if (index < array_content.length) {
-											csvContent += dataString+ "\n";
+											full_data_csv += dataString+ "\n";
 										 } else{
-											csvContent += dataString;
+											full_data_csv += dataString;
 										 }
 								});
-								var export_full_data_csv = encodeURI(csvContent);
-								// Display btn for export 
+								var export_full_data_csv = encodeURI(full_data_csv);
 								jQuery('#export_full_data_csv').attr('href',export_full_data_csv);
+								// Pattern Full Detail
+								var full_detail_csv = "data:text/csv;charset=utf-8,";
+								array_content.forEach(function(infoArray, index){
+									if (index != 0) {
+										var url_data = infoArray[1];
+										jQuery.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent(url_data) + '&callback=?', function(data){
+											var data_set  = jQuery.parseHTML(data.contents);
+											var wrapper_data = jQuery(data_set[49]);
+											var text_data = wrapper_data.find('.post-wrap').text();
+											var parent_set = jQuery('#list_full_detail_csv tbody');
+											var html_pattern = '';
+													html_pattern += '<tr>';
+													html_pattern += '<td>';
+													html_pattern += '<a href="'+url_data+'" target="_blank">'+url_data+'</a>';
+													html_pattern += '</td>';
+													html_pattern += '<td>';
+													html_pattern += text_data;
+													html_pattern += '</td>';
+													html_pattern += '</tr>';
+											parent_set.append(html_pattern);
+											full_detail_csv += url_data+','+text_data+'\n';
+											var export_full_detail_csv = encodeURI(full_detail_csv);
+											jQuery('#export_full_detail_csv').attr('href',export_full_detail_csv);
+										});
+									}
+								});
+								// Display btn for export 
 								jQuery('#generate_csv').hide();
-								jQuery('#export_csv').show();
+								jQuery('.sk-fading-circle').show('slow',function() {
+									jQuery('#export_csv').delay(2000).show('slow',function() {
+										jQuery('.sk-fading-circle').hide('fast');
+									});
+								});
 					}
 				}
 			});
